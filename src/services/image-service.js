c9a4f60e852acf7c6b3f6d7c8c44a226e52083bb
bonny-art <svitlana.otenko@gmail.com>
2023-12-26 14:13:34 +0200

    Update image-service.js

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = '40358053-aa77a52d7b78d629a29aff12d';
 export const PER_PAGE = 12;
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['Authorization'] = API_KEY;
+
 axios.defaults.params = {
   orientation: 'horizontal',
   per_page: PER_PAGE,
